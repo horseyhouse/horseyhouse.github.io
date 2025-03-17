56e413b058aa7013795188c1d51f78974bce1ebd
theia <theia@mit.edu>
2025-03-17 11:37:16 -0400

    removd old login components

--- components/LoginPage.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-import { GraffitiIdentityProviderLogin } from "@graffiti-garden/client-vue";
-import "@graffiti-garden/client-vue/dist/style.css";
-</script>
-
-<template>
-    <GraffitiIdentityProviderLogin client-name="horsey.house" />
-</template>
